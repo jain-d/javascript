2511cf2504a23e59b286079e856c38f743731acf
jain-d <jain.divyansh.0017@gmail.com>
2023-04-21 20:39:12 +0530

    system arch changes

--- systemArch/script.js
+++ systemArch/script.js
@@ -1,12 +1,13 @@
 let text=document.querySelector('#text')
 let btn=document.querySelector('#clk')
-let data= navigator.userAgentData.getHighEntropyValues(["platform"]);
+let data= navigator.userAgentData.getHighEntropyValues(["architecture","bitness"]);
 data.then((result)=>{
-    ans=result.platform;
+    chip=result.architecture;
+    bitness=result.bitness;
 })
 if(btn){
     btn.addEventListener('click', ()=>{
-        text.innerHTML=ans;
+        text.innerHTML=chip+bitness+' chip';
         console.log(data)
     })
 }
