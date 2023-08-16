fa7a5d6734ad0eb7327aa996333633e27ca22baf
Divyanshu <divyanshu.jain@neudesic.com>
2023-08-17 03:13:56 +0530

    cleaning up

--- keypress/script.js
+++
@@ -1,9 +0,0 @@
-// let btn = document.querySelector('button');
-// if (btn) {
-//     btn.addEventListener('click',()=>{console.log("Yes it is !")})
-// }
-let keypress = document.querySelector('.keypress');
-
-document.addEventListener('keydown', (event)=>{
-    keypress.innerHTML = event.key;
-})
