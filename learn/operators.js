7e523ec1798f1b05f60d6be3c140c3c505ebe978
jain-d <jain.divyansh.0017@gmail.com>
2024-12-18 20:53:57 +0530

    logical operators

---
+++ learn/operators.js
@@ -0,0 +1,24 @@
+// OPERATORS
+
+// Logical Operator     &&, || and ??
+//
+/* Apart from being used in conditionals, these logical operators are also used in expression part of an assignment, and this is where it can get confusing if not understood entirely. */
+
+// AND && operator
+/* The AND operator, when used as an expression, returns the last truthy value, or if it short circuits returns the current false value */
+let monkey = true && 3 && "banana";          // <-- monkey variable stores "banana", because it is the LAST truthy value
+monkey = true && 0 && "banana";              // <-- here, true and "banana" are both truthy, but the value stored in monkey will be 0, the current falsy value
+monkey = false && 0 && "";                   // <-- now all values are falsy, but the operator will short circuit the first time it encounters a falsy value, which in this case is the first value itself. So the value monkey will hold is false.
+
+// OR || operator
+/* The OR operator behaves exactly OPPOSITE of the AND operator. It will return the last falsy value, or if it short circuits returns the first truthy value */
+monkey = true || 3 || "banana";              // <-- now here, as soon as we encounter the first truthy value, OR operators short circuits and returns that truthy value. in this case, it is true.
+monkey = false || 3 || "banana";             // <-- the first truthy value is 3, the value after this, "banana" is also truthy but we wont' reach there because, again, OR short-circuits at 3 and returns that.
+monkey = false || 0 || "";                   // <-- this is where we go all the way till the end, because OR can not find a truthy value. And the last value "" is also a falsy and this is what it returned.
+
+
+// NULLISH COALESCING ?? operator
+/* The NULLISH COALESCING operator looks for the first value this is neither NULL nor UNDEFINED. It short circuits when it finds one, and returns that value. Else it returns the last undefined or null. */
+monkey = 0 ?? true ?? "banana";              // <-- this will return 0 as the value because that the first value that is neither null nor undefined.
+monkey = null ?? false ?? "banana";          // <-- this will return false as the value and the operator will short-circuit here.
+monkey = null ?? undefined ?? null;          // <-- now here, the operator is looking for a value that is neither null nor undefined, but since it can not find it, it will return the last value it checked, which is null here.
